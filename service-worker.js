28f78328c2c8b56a29314cf8e1e123969ae77871
Ramsay Smith <ramsaysmith@outlook.com>
2022-03-24 21:53:36 -0500

    move manifest and sw to root

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function (event) {
 			return cache.addAll([
 				'/index.html',
 				'/styles.css',
-				'/pwa/manifest.webmanifest',
+				'/manifest.webmanifest',
 				'/assets/',
 			]);
 		}),
