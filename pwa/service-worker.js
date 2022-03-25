fa6a89c7579fcf12c3becda86d9d88867bedef30
Ramsay Smith <ramsaysmith@outlook.com>
2022-03-24 21:47:28 -0500

    initial manifest and sw

---
+++ pwa/service-worker.js
@@ -0,0 +1,40 @@
+self.addEventListener('install', function (event) {
+	event.waitUntil(
+		caches.open('image-tester-cache').then(function (cache) {
+			return cache.addAll([
+				'/index.html',
+				'/styles.css',
+				'/pwa/manifest.webmanifest',
+				'/assets/',
+			]);
+		}),
+	);
+});
+
+// Clean up caches if needed
+self.addEventListener('activate', function (event) {
+	event.waitUntil(
+		caches.keys().then(function (cacheNames) {
+			return Promise.all(
+				cacheNames
+					.filter(function (cacheName) {
+						// Return true if you want to remove this cache,
+						// but remember that caches are shared across
+						// the whole origin
+					})
+					.map(function (cacheName) {
+						return caches.delete(cacheName);
+					}),
+			);
+		}),
+	);
+});
+
+// Cache first, then network
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		}),
+	);
+});
